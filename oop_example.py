ac084a6247dca0021a7a6c6c73144e693e5d0a72
Yulya <yulya.yy@gmail.com>
2020-01-11 17:04:18 +0100

    class Koala added

--- oop_example.py
+++ oop_example.py
@@ -12,6 +12,22 @@ class Panda:
     def __str__(self):
         return "This is a Panda called " + self.nom + ", and its weight is " + str(self.weight) + "kg"
 
+class Koala:
+    def __init__(self, name):
+        self.name = str(name) 
+        self.weight = 0.2
+        print("New Koala "+ self.name+ " was born")
+    
+    def fressen(self, food_mass):
+        self.weight = self.weight + food_mass/4
+
+koala_1 = Koala("Koala 1")
+print(koala_1.weight)
+koala_2 = Koala("Koala 2")
+koala_1.fressen(0.1)
+print(koala_1.weight)
+
+
 a = Panda("Lu")
 b = Panda("Qu")
 c = Panda(3.14)
@@ -20,3 +36,4 @@ print(a)
 a.fressen(0.5)
 a.fressen(1)
 print(a)
+
