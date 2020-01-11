cb7e3722b4093bb109673acb51cd1f5a92e852bb
Yulya <yulya.yy@gmail.com>
2020-01-11 17:39:47 +0100

    inheritance with class Animal

--- oop_example.py
+++ oop_example.py
@@ -1,39 +1,39 @@
 import random
 
-class Panda:
+class Animal:
+    def __init__(self):
+        self.k = 1
+    def fressen(self, food_mass):
+        self.weight = self.weight + food_mass * self.k
+
+class Panda(Animal):
     all_pandas = []
     def __init__(self, name):
+        Animal.k = 0.5
         self.nom = str(name)
         self.weight = 1
         Panda.all_pandas.append(self.nom)
         print("New Panda "+ self.nom)
 
-    def fressen(self, food_mass):
-        self.weight = self.weight + food_mass/2
-
     def __str__(self):
         return "This is a Panda called " + self.nom + ", and its weight is " + str(self.weight) + "kg"
 
-class Koala:
+class Koala(Animal):
     def __init__(self, name):
+        Animal.k = 0.25
         self.name = str(name) 
         self.weight = 0.2
         print("New Koala "+ self.name+ " was born")
-    
-    def fressen(self, food_mass):
-        self.weight = self.weight + food_mass/4
 
-class Snake:
+class Snake(Animal):
     def __init__(self, given_name):
+        Animal.k = 0.1
         self.name = given_name
-        self.l = (random.random() + 1)/2 * 3.14
-
-    def fressen(self, food_mass):
-        self.l = self.l + food_mass/10
+        self.weight = (random.random() + 1)/2 * 3.14
 
     def __str__(self):
         s = "="
-        s = s*int(self.l*10)
+        s = s*int(self.weight*10)
         return self.name+"  <<"+s+"§--´"
 
 pipi = Snake("PiPi")
