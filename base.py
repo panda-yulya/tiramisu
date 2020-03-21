a07ff378228b21bde4de2b47eeb5c42fd99dff71
Yulya <yulya.yy@gmail.com>
2020-03-21 15:46:15 +0100

    funktion read_image

--- base.py
+++ base.py
@@ -3,15 +3,19 @@ import socketserver
 
 PORT = 80
 
+def read_image():
+    i = None
+    with open("C:\\Users\\yuliy\\Documents\\dev\\tiramisu\\iam.jpg", "rb") as f:
+        i = f.read()
+    return i    
+
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        img = None
-        with open("C:\\Users\\yuliy\\Documents\\dev\\tiramisu\\iam.jpg", "rb") as f:
-            img = f.read()
+        img = read_image()
         self.wfile.write(img)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
