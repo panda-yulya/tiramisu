0842af1ac850657bebb172caf46d3f7d3563b406
Yulya <yulya.yy@gmail.com>
2020-03-21 17:41:28 +0100

    do_get returns conditional response

--- base.py
+++ base.py
@@ -9,14 +9,36 @@ def read_image():
         i = f.read()
     return i    
 
-
 class Handler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        img = read_image()
-        self.wfile.write(img)
+        #img = read_image()
+        text = b"""<html>
+        Hi, Pavel
+        <img src="iam.jpg">
+        </html>"""
+        if self.path == "/":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(text) 
+        elif self.path == "/iam.jpg":
+            self.send_response(200)
+            self.end_headers()
+            img = read_image()
+            self.wfile.write(img)
+        elif self.path == "/favicon.ico":
+            self.send_response(200)
+            self.end_headers()
+            img = read_image()
+            self.wfile.write(img)
+        else:
+            self.send_response(404)
+            self.end_headers()
+            print("stop")
+            
+
+        #print("request ok %s" % (self.path))
+     
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
